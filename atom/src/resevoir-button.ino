db311dadecb336d18ac96ce35ded16a4b5ff9d12
Peter Buelow <peter.buelow@lifefitness.com>
2018-07-07 06:53:12 -0500

    Missed updates to the Photon source

--- atom/src/resevoir-button.ino
+++ atom/src/resevoir-button.ino
@@ -6,8 +6,10 @@
  */
 #include "MQTT.h"
 #include "InternetButton.h"
+#include "FastLED.h"
 
-#define APP_ID  10
+#define APP_ID  11
+FASTLED_USING_NAMESPACE;
 
 void subCallback(char*, byte*, unsigned int);
 
@@ -16,42 +18,79 @@ InternetButton btn = InternetButton();
 bool g_connected;
 bool g_pumpOn;
 bool g_valveOpen;
+bool g_receivedPing;
+bool g_relayOn;
 unsigned long g_lockout;
+int g_missedPingCount;
+
+#define TEST_RELAY_ON   0x00000001
+#define TEST_VALVE_ON   0x00000010
+#define TEST_PUMP_ON    0x00000100
 
 void subCallback(char* t, byte* p, unsigned int length)
 {
     String topic = String(t);
 
-    if (topic == "control/pump/on") {
-        btn.ledOn(3, 0, 100, 0);
+    if (topic == "aquarium/pump/on") {
+        btn.ledOn(3, 0, 255, 0);
         g_pumpOn = true;
     }
-    if (topic == "control/pump/off") {
-        btn.ledOn(3, 100, 0, 0);
+    if (topic == "aquarium/pump/off") {
+        btn.ledOn(3, 255, 0, 0);
         g_pumpOn = false;
     }
-    if (topic == "control/valve/open") {
-        btn.ledOn(9, 0, 100, 0);
+    if (topic == "aquarium/valve/open") {
+        btn.ledOn(6, 0, 255, 0);
         g_valveOpen = true;
     }
-    if (topic == "control/valve/close") {
-        btn.ledOn(9, 100, 0, 0);
+    if (topic == "aquarium/valve/close") {
+        btn.ledOn(6, 255, 0, 0);
         g_valveOpen = false;
     }
-    if (topic == "control/state") {
-        g_pumpOn = false;
-        g_valveOpen = false;
-        btn.ledOn(3, 100, 0, 0);
-        btn.ledOn(9, 100, 0, 0);
-        if (p[0] == '1') {
+    if (topic == "aquarium/relay/on") {
+        btn.ledOn(0, 0, 255, 0);
+        g_relayOn = true;
+    }
+    if (topic == "aquarium/relay/off") {
+        btn.ledOn(0, 255, 0, 0);
+        g_relayOn = false;
+    }
+    if (topic == "aquarium/state") {
+        if (*p & TEST_RELAY_ON) {
+            g_relayOn = true;
+            btn.ledOn(0, 0, 255, 0);
+        }
+        else {
+            g_relayOn = false;
+            btn.ledOn(0, 255, 0, 0);
+        }
+        if (*p & TEST_PUMP_ON) {
             g_pumpOn = true;
-            btn.ledOn(3, 0, 100, 0);
+            btn.ledOn(3, 0, 255, 0);
         }
-        if (p[1] == '1') {
+        else {
+            g_pumpOn = false;
+            btn.ledOn(3, 255, 0, 0);
+        }
+        if (*p & TEST_VALVE_ON) {
             g_valveOpen = true;
-            btn.ledOn(9, 0, 100, 0);
+            btn.ledOn(6, 0, 255, 0);
+        }
+        else {
+            g_valveOpen = false;
+            btn.ledOn(6, 255, 0, 0);
         }
     }
+    if (topic == "aquarium/pong") {
+        btn.ledOn(1, 0, 100, 100);
+        g_receivedPing = true;
+        g_missedPingCount = 0;
+    }
+}
+
+void sendPing()
+{
+    client.publish("aquarium/ping", "");
 }
 
 // setup() runs once, when the device is first turned on.
@@ -62,7 +101,10 @@ void setup()
     g_connected = false;
     g_pumpOn = false;
     g_valveOpen = false;
+    g_receivedPing = false;
+    g_relayOn - false;
     g_lockout = 0;
+    g_missedPingCount = 0;
 
     Particle.variable("appid", appId);
 
@@ -70,26 +112,37 @@ void setup()
 
     client.connect("buttons");
     if (client.isConnected()) {
-        client.subscribe("control/#");
-        client.publish("resevoir/state", "");
+        client.subscribe("aquarium/#");
         btn.allLedsOn(0,100,0);
         delay(3000);
         btn.allLedsOff();
         g_connected = true;
+        btn.ledOn(11, 100, 100, 100);
     }
     else {
-        btn.allLedsOn(100, 0, 0);
+        btn.allLedsOn(255, 0, 0);
         g_connected = false;
     }
+
+    client.publish("aquarium/status", "");
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop()
 {
-    if (client.isConnected())
+    if (client.isConnected()) {
+        EVERY_N_MILLISECONDS(1000) {
+            sendPing();
+            if (!g_receivedPing) {
+                g_missedPingCount++;
+            }
+            if (g_receivedPing)
+                g_receivedPing = false;
+        }
         client.loop();
+    }
     else {
-        btn.allLedsOn(100, 0, 0);
+        btn.allLedsOn(255, 0, 0);
     }
 
     if (g_lockout != 0) {
@@ -101,18 +154,29 @@ void loop()
         }
     }
 
+    if (g_missedPingCount > 10) {
+        btn.ledOn(1, 255, 0, 0);
+    }
+
+    if (btn.buttonOn(1)) {
+        g_lockout = millis();
+        if (g_relayOn)
+            client.publish("aquarium/relay/off", "");
+        else
+            client.publish("aquarium/relay/on", "");
+    }
     if (btn.buttonOn(2)) {
         g_lockout = millis();
-        if (!g_pumpOn)
-            client.publish("resevoir/pump/on", "");
+        if (g_pumpOn)
+            client.publish("aquarium/pump/off", "");
         else
-            client.publish("resevoir/pump/off", "");
+            client.publish("aquarium/pump/on", "");
     }
-    if (btn.buttonOn(4)) {
+    if (btn.buttonOn(3)) {
         g_lockout = millis();
-        if (!g_valveOpen)
-            client.publish("resevoir/valve/open", "");
+        if (g_valveOpen)
+            client.publish("aquarium/valve/close", "");
         else
-            client.publish("resevoir/valve/close", "");
+            client.publish("aquarium/valve/open", "");
     }
 }
